198d9c2de81cd22904a66362f548e137f8a7ce06
fabeyor <fabeyor@gmail.com>
2023-05-05 19:26:13 +0100

    Update secpasswdgen.sh

--- secpasswdgen.sh
+++ secpasswdgen.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-#Secure Random Password Generator (SRPG)
-#Author: Samuel fabeyo
-#Website: https://fabeyor.medium.com
-
+# Secure Random Password Generator (SRPG)
+# Author: Samuel Fabeyo
+# Website: https://fabeyor.medium.com
+# GitHub: https://github.com/fabeyor
 
 echo "Welcome to the Secure Random Password Generator (SRPG)"
+echo "______________________________________________________"
 
+function secure_password() {
+    # Generates random passwords in the length of choice of the user.
 
-function secure_password(){
-    #Generates random passwords in the lenght of choice of the user.
+    read -p "Please enter the desired password length: " password_length
 
-    echo "Please, how many password characters do you want to generate? "
+    # Define minimum password length.
+    minimum_password_length=16
 
-    MINI="12"
-    read -r PASSWD_LENGHT
+    # Checks if the password length is at least 16 characters and generates a password.
+    if [ -z "${password_length}" ] || [ "${password_length}" -lt "${minimum_password_length}" ]; then
+        echo "_________________________________________________________________"
+        echo "Please, the minimum password length is ${minimum_password_length}"
+        echo "_________________________________________________________________"
+        exit 1 
+    else
+        # Generate the password.
+        for character in $(seq 1); do
+            PASSWORD=$(head /dev/urandom | LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&\x27()*+,-./:;<=>?@[\\]^_`{|}~' | head -c"${password_length}" ; echo '')
 
-    #Checks if the password lenght is atleast 12 Characters.
-
-    if  [ -z $PASSWD_LENGHT ] &&  [ "${#PASSWD_LENGHT}" -ge "$MINI" ];
-    then
-        for CHARACTER in $(seq 1); do
-            openssl rand -hex && openssl rand -base64 48 | cut -c1-"$PASSWD_LENGHT" | shuf
-    #else
-        #echo "Please, the minimum password lenght is $MINI"
-        done 
+            # Print the password securely.
+            echo "_____________________"
+            echo "Your new password is:"
+            echo "_____________________"
+            printf '%s\n' "$PASSWORD"
+        done
     fi           
 }
 
-
-function random_pass (){
-    #Generates a random pass using openssl
-    #read PASSWD_LENGHT
-    for CHARACTER in $(seq 1); 
-    do
-    openssl rand -hex && openssl rand -base64 48 | cut -c1-"$PASSWD_LENGHT" | shuf
-    done
-}
-random_pass
 secure_password
